27216a3d1bda20da6233bbaa18481b7d88030bd3
sky121016 <sky121016@naver.com>
2023-10-11 14:42:53 +0900

    Description Components

--- week3/netflix-clone/src/App.js
+++ week3/netflix-clone/src/App.js
@@ -6,11 +6,10 @@ function App() {
   return (
     <div className="App">
       <div className="app-container">
-        {movies.results.map((item)=>{
+        {movies.results.map((item, index)=>{
           return(
-            <Movie poster_path={item.poster_path} title={item.title} vote_average={item.vote_average}/>
+            <Movie poster_path={item.poster_path} title={item.title} vote_average={item.vote_average} overview={item.overview} key={index}/>
           )
-
         })}
       </div>
     </div> 
